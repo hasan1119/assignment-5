65792c50ee9a69dd7e4d51ec78c96e3a9f38ddcc
Md. Rokibul Hasan <hasan.webappsdeveloper@gmail.com>
2021-08-21 15:35:25 +0600

    finished

--- js/app.js
+++ js/app.js
@@ -1,71 +1,84 @@
+//8GB unified memory
 document.getElementById('memory-8gb').addEventListener('click', function () {
-    updateValue('memory-cost', 0)
-
+    updateExtraCostValue('memory-cost', 0)
 })
 
+//16GB unified memory
 document.getElementById('memory-16gb').addEventListener('click', function () {
-    updateValue('memory-cost', 180)
+    updateExtraCostValue('memory-cost', 180)
 })
 
+//256GB SSD storage
 document.getElementById('storage-256gb').addEventListener('click', function () {
-    updateValue('storage-cost', 0)
+    updateExtraCostValue('storage-cost', 0)
 })
 
+//512GB SSD storage
 document.getElementById('storage-512gb').addEventListener('click', function () {
-    updateValue('storage-cost', 100)
+    updateExtraCostValue('storage-cost', 100)
 })
 
+//1TB SSD storage
 document.getElementById('storage-1tb').addEventListener('click', function () {
-    updateValue('storage-cost', 180)
+    updateExtraCostValue('storage-cost', 180)
 })
 
+//Free delivery
 document.getElementById('free-delivery').addEventListener('click', function () {
-    updateValue('delivery-cost', 0)
+    updateExtraCostValue('delivery-cost', 0)
 })
 
+//Paid delivery
 document.getElementById('paid-delivery').addEventListener('click', function () {
-    updateValue('delivery-cost', 20)
+    updateExtraCostValue('delivery-cost', 20)
 })
 
 
-
-function updateValue(id, value) {
-    var costElement = document.getElementById(id)
+//Function for updating extra-cost amount
+function updateExtraCostValue(id, value) {
+    const costElement = document.getElementById(id)
     costElement.innerText = value;
-
-    let bestPriceElement = document.getElementById('best-price')
-    let bestPrice = Number(bestPriceElement.innerText)
-
-    let extraMemoryElement = document.getElementById('memory-cost')
-    let extraMemoryCost = Number(extraMemoryElement.innerText)
-
-    let extraStorageElement = document.getElementById('storage-cost')
-    let extraStorageCost = Number(extraStorageElement.innerText)
-
-
-    let extraDeliveryElement = document.getElementById('delivery-cost')
-    let extraDeliveryCost = Number(extraDeliveryElement.innerText)
-
-    let extraTotalCost = bestPrice + extraMemoryCost + extraStorageCost + extraDeliveryCost;
-
-    let extraTotalElement = document.getElementById('extra-total-cost')
-    let inTotalElement = document.getElementById('in-total')
-    extraTotalElement.innerText = extraTotalCost;
-    inTotalElement.innerText = extraTotalCost;
-
+    updateTotal() //calling the totalAmount function
 }
 
+
+//Function for updating total Amount
+function updateTotal() {
+    const bestPriceElement = document.getElementById('best-price')
+    const bestPrice = Number(bestPriceElement.innerText)
+
+    const extraMemoryElement = document.getElementById('memory-cost')
+    const extraMemoryCost = Number(extraMemoryElement.innerText)
+
+    const extraStorageElement = document.getElementById('storage-cost')
+    const extraStorageCost = Number(extraStorageElement.innerText)
+
+    const extraDeliveryElement = document.getElementById('delivery-cost')
+    const extraDeliveryCost = Number(extraDeliveryElement.innerText)
+
+    const extraTotalCost = bestPrice + extraMemoryCost + extraStorageCost + extraDeliveryCost;
+
+    const extraTotalElement = document.getElementById('extra-total-cost')
+    const inTotalElement = document.getElementById('in-total')
+
+    extraTotalElement.innerText = extraTotalCost;
+    inTotalElement.innerText = extraTotalCost;
+}
+
+// Promo Apply event handler
 document.getElementById('apply-btn').addEventListener('click', function () {
-    let totalElement = document.getElementById('extra-total-cost')
-    let inTotalElement = document.getElementById('in-total')
-    let promoInput = document.getElementById('promo-code')
-    let promoValue = promoInput.value
-    let totalAmount = Number(totalElement.innerText)
+    const totalElement = document.getElementById('extra-total-cost')
+    const inTotalElement = document.getElementById('in-total')
+    const promoInput = document.getElementById('promo-code')
+    const promoValue = promoInput.value
+    const totalAmount = Number(totalElement.innerText)
 
     if (promoValue == 'stevekaku') {
-        let discount = (totalAmount / 100) * 20;
+        const discount = (totalAmount / 100) * 20;
         inTotalElement.innerText = totalAmount - discount;
+        document.getElementById('wrong-promo').classList.add("d-none")
+    } else {
+        document.getElementById('wrong-promo').classList.remove("d-none")
     }
-
-    promoInput.value = ''
+    promoInput.value = '' // clearing the promo input section
 })
